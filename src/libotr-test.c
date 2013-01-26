beae608fe15085084361ec1534803fe8b972905e
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2013-01-26 12:39:01 +0300

    updated libotr-test.c

--- src/libotr-test.c
+++ src/libotr-test.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <gcrypt.h>
 #include <libotr/proto.h>
 #include <libotr/userstate.h>
@@ -22,12 +23,12 @@ test_generate_key(OtrlUserState userstate, const char* keys, const char* account
     char fingerprint[45];
 
     puts("generating key");
-    gcry_error_t error = otrl_privkey_generate(userstate, "bob.keys","bob@telechat.org","telechat");
+    gcry_error_t error = otrl_privkey_generate(userstate, keys,accountname,protocol);
 
     if(error){
-		printf("test_generate_key FAILED.\n");
+		printf("test_generate_key failed: %s\n",gcry_strerror(error));
 	}else{
-        otrl_privkey_fingerprint(userstate, fingerprint, "bob@telechat.org", "telechat");
+        otrl_privkey_fingerprint(userstate, fingerprint, accountname,protocol);
         printf("fingerprint of generated key: %s\n",fingerprint);
     }
     return error;
@@ -40,32 +41,22 @@ test_read_file(OtrlUserState userstate, const char* keys, const char* accountnam
 	gcry_error_t error = otrl_privkey_read(userstate, keys);
 
 	if(error){
-		printf("test_read_file FAILED.\n");
+		printf("test_read_file failed: %s\n",gcry_strerror(error));
 	}else{
     	otrl_privkey_fingerprint(userstate, fingerprint, "alice@telechat.org", "telechat");
         printf("fingerprint: %s\n",fingerprint);
     }
     return error;
 }
-void
-test_mpi_to_bigint_conversions(int T){
-    gcry_mpi_t u = gcry_mpi_set_ui(NULL,2);
-    gcry_mpi_t w = gcry_mpi_set_ui(NULL,1);
-    int i;
-    for(i=0; i<T; i++) {
-        gcry_mpi_mul(w,w,u);
-    }
-    gcry_mpi_dump(w);
-}
 
 int
 main(){
     initialise();
+    printf("libotr %s\n",otrl_version());
     OtrlUserState userstate = otrl_userstate_create();
-
-    test_read_file(userstate,"/keys/alice.keys","alice@telechat.org","telechat");
-    test_generate_key(userstate,"/keys/bob.keys","bob@telechat.org","telechat");
-
+    test_read_file(userstate,"./keys/alice.keys","alice@telechat.org","telechat");
+    test_generate_key(userstate,"./keys/alice.keys","alice@telechat.org","telechat");
+    test_read_file(userstate,"./keys/alice.keys","alice@telechat.org","telechat");
     otrl_userstate_free(userstate);
     return 0;
 }
