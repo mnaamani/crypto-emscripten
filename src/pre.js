73577cf8ec30aaa470c48d9a17add9062a5c6529
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2013-07-08 19:14:24 +0300

    no need for random.js

--- src/pre.js
+++ src/pre.js
@@ -1,4 +1,7 @@
-Module["Random"] = this["Random"] || require("random"); 
+try{
+    Module["crypto"] = this["crypto"] || require("crypto");
+}catch(e){}
+
 Module["preRun"] = Module["preRun"] || [];
 
 (function(){
@@ -11,11 +14,21 @@ Module['preRun'].push(function(){
     FS.init();
     var devFolder = FS.findObject("/dev") || Module['FS_createFolder']("/","dev",true,true);
 
-    Module['FS_createDevice'](devFolder,"random",(function(){
-      return Module["Random"]["getByte"]();
-    }));
-
-    Module['FS_createDevice'](devFolder,"urandom",(function(){
-      return Module["Random"]["getByte"]();
-    }));
+    var randomByte = (function(crypto){
+       if(!crypto) throw new Error("no source of random data found!");
+       if(crypto["randomBytes"]) {
+          return (function(){
+            return crypto["randomBytes"](1)[0];
+          });
+       }else{
+         return (function(){
+            var buf = new Uint8Array(1);
+            crypto["getRandomValues"](buf);
+            return buf[0];
+         });
+       }
+    })(Module["crypto"]);
+    
+    Module['FS_createDevice'](devFolder,"random",randomByte);
+    Module['FS_createDevice'](devFolder,"urandom",randomByte);
 });
