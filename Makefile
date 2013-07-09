649337c9c058f4034561ffd0711c410a9d2a20c3
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2013-07-10 02:30:48 +0300

    fast_mpi working with asm.js and closure

--- Makefile
+++ Makefile
@@ -7,8 +7,9 @@ OTR= -lotr
 NODE= --pre-js ./src/pre-node.js
 PRE= --pre-js ./src/pre.js --pre-js ./src/fast_mpi.js
 #see notes.txt for best options to use
-OPTIMISATION= -O2 --llvm-opts 1 --llvm-lto 0 -s ASM_JS=0 --closure 0
-WEB= -O1 --llvm-opts 1 --llvm-lto 0 -s ASM_JS=0 --closure 0
+OPTIMISATION= -O2 --llvm-opts 1 --llvm-lto 0 -s ASM_JS=1 --closure 1 \
+    -s EXPORTED_FUNCTIONS="['_main','_malloc','_free','__gcry_strerror','__gcry_mpi_new','__gcry_mpi_set','__gcry_mpi_release', \
+            '__gcry_mpi_scan','__gcry_mpi_print']" -s IGNORED_FUNCTIONS="['__gcry_mpi_powm','__gcry_mpi_mulpowm','__gcry_mpi_invmod']"
 
 TEST_OBJS=benchmark.js basic.js pubkey.js keygen.js prime.js ac-data.js ac.js ac-schemes.js curves.js \
     fips186-dsa.js fipsdrv.js hmac.js mpitests.js pkcs1v2.js random.js register.js rsacvt.js t-kdf.js \
@@ -44,5 +45,4 @@ clean:
 
 web:
 	mkdir -p tests/
-	$(EMCC) $(GCRYPT_BUILD)/tests/basic.o -o tests/basic.html $(LIBS) $(GCRYPT) $(WEB) $(PRE) --shell-file ./src/shell.html
-
+	$(EMCC) $(GCRYPT_BUILD)/tests/basic.o -o tests/basic.html $(LIBS) $(GCRYPT) $(OPTIMISATION) $(PRE) --shell-file ./src/shell.html
--- src/fast_mpi.js
+++ src/fast_mpi.js
@@ -17,117 +17,103 @@
  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-try{
-Module["BigInt"]= this["BigInt"] || require("bigint");
-}catch(e){}
+//TODO: turn this into a library...
+
+(function(){
+    Module["BigInt"]= this["BigInt"] || require("bigint");
+
+    /* convert a gcry_mpi_t to a BigInt */
+    function mpi2bigint(mpi_ptr){
+        if(!this.buffer) this.buffer = Module["_malloc"](4096);
+        //gcry_error_t gcry_mpi_print (enum gcry_mpi_format format, unsigned char *buffer, size_t buflen, size_t *nwritten, const gcry_mpi_t a)
+        var err = Module["libgcrypt"]["mpi_print"](4,this.buffer,4096,0,mpi_ptr);// 4 == HEX Format
+        assert(err==0,"mpi2bigint"+Module["libgcrypt"]["strerror"](err));
+        var mpi_str = Module["Pointer_stringify"](this.buffer);
+        return Module["BigInt"]["str2bigInt"](mpi_str,16);
+    }
+
+     /* convert bi_num (BigInt) to hex string then scan it into a new gcry_mpi_t, using gcry_mpi_scan
+        copy the new mpi to mpi_ptr */
+    function bigint2mpi(mpi_ptr,bi_num){
+        if(!this.handle) this.handle = Module["_malloc"](4);//pointer to *gcry_mpi_t
+        var bi_num_str = Module["BigInt"]["bigInt2str"](bi_num,16);
+        //gcry_error_t gcry_mpi_scan (gcry_mpi_t *r_mpi, enum gcry_mpi_format format, const unsigned char *buffer, size_t buflen, size_t *nscanned)
+        var err = Module["libgcrypt"]["mpi_scan"](this.handle,4,bi_num_str,0,0); //4 == HEX Format
+        assert(err===0,"bigint2mpi:"+Module["libgcrypt"]["strerror"](err));
+    
+        var scanned_mpi_ptr = Module["getValue"](this.handle,"i32");
+        assert(scanned_mpi_ptr !==0, "NULL scanned mpi in bigint2mpi()");
+    
+        Module["libgcrypt"]["mpi_set"](mpi_ptr,scanned_mpi_ptr);
+        Module["libgcrypt"]["mpi_release"](scanned_mpi_ptr);
+    }
+
+    function override_mpi_powm(mpi_w,mpi_b,mpi_e,mpi_m){
+          //w = b^e mod m
+          var bi_base = mpi2bigint(mpi_b);
+          var bi_expo = mpi2bigint(mpi_e);
+          var bi_mod  = mpi2bigint(mpi_m);
+          var result = Module["BigInt"]["powMod"](bi_base,bi_expo,bi_mod);
+          bigint2mpi(mpi_w,result);
+    }
+
+    function override_mpi_mulpowm(mpi_r,mpi_array_base,mpi_array_exp,mpi_m){
+        var indexer = 1;
+        var mpi1, mpi2, bi_m,bi_result;
+        mpi1 = Module["getValue"](mpi_array_base,"i32");
+        mpi2 = Module["getValue"](mpi_array_exp,"i32");
+        bi_m = mpi2bigint(mpi_m);
+        var BE = [];
+        var O = [];
+        while(mpi1 && mpi2){
+            BE.push({"b":mpi2bigint(mpi1),"e":mpi2bigint(mpi2)});
+            mpi1 = Module["getValue"](mpi_array_base+(indexer*4),"i32");
+            mpi2 = Module["getValue"](mpi_array_exp+ (indexer*4),"i32");
+            indexer++;
+        }
+        if(BE.length){
+            BE.forEach(function(be){
+                O.push(Module["BigInt"]["powMod"](be["b"],be["e"],bi_m));
+            });
+            bi_result = Module["BigInt"]["str2bigInt"]("1",16);
+            O.forEach(function(k){
+                bi_result = Module["BigInt"]["mult"](bi_result,k);
+            });
+        }
+        bi_result = Module["BigInt"]["mod"](bi_result,bi_m);
+        bigint2mpi(mpi_r,bi_result);
+    }
+
+    function override_mpi_invm(mpi_x,mpi_a,mpi_m){
+        // (x**(-1) mod n)
+        var bi_a = mpi2bigint(mpi_a);
+        var bi_m = mpi2bigint(mpi_m);
+        var result = Module["BigInt"]["inverseMod"](bi_a,bi_m);
+        if(result){
+            bigint2mpi(mpi_x,result);
+            return 1;//inverse mod calculated
+        }else{
+            return 0;//no inverse mod exists
+        }
+    }
+
+    this["__gcry_mpi_powm"]= Module['__gcry_mpi_powm_override'] = override_mpi_powm;
+    this["__gcry_mpi_mulpowm"]= Module['__gcry_mpi_mulpowm_override'] = override_mpi_mulpowm;
+    this["__gcry_mpi_invm"]= Module['__gcry_mpi_invm_override'] = override_mpi_invm;
+
+}).call(globalScope);
 
 Module['preRun'].push(function(){
-    if(typeof asm !== 'undefined') return;
-    var gcry_ = {};
-    var BigInt = Module["BigInt"];
-    /*
-     * convert a gcry_mpi_t to a BigInt
-     */
-    function mpi2bigint(mpi_ptr){
-        if(!mpi2bigint.buffer) mpi2bigint.buffer = _malloc(4096);
-        //gcry_error_t gcry_mpi_print (enum gcry_mpi_format format, unsigned char *buffer, size_t buflen, size_t *nwritten, const gcry_mpi_t a)
-        var err = gcry_.mpi_print(4,mpi2bigint.buffer,4096,0,mpi_ptr);// 4 == HEX Format
-        assert(err==0,"mpi2bigint"+gcry_.strerror(err));
-        var mpi_str = Module['Pointer_stringify'](mpi2bigint.buffer);
-        return BigInt["str2bigInt"](mpi_str,16);
-    }
+    Module["libgcrypt"] = {
+        "mpi_new":Module["cwrap"]('_gcry_mpi_new','number',['number']),
+        "mpi_set":Module["cwrap"]('_gcry_mpi_set','number',['number','number']),
+        "mpi_release":Module["cwrap"]('_gcry_mpi_release','',['number']),
+        "mpi_scan":Module["cwrap"]('_gcry_mpi_scan','number',['number','number','string','number','number']),
+        "mpi_print":Module["cwrap"]('_gcry_mpi_print','number',['number','number','number','number','number']),
+        "strerror":Module["cwrap"]('_gcry_strerror','string',['number'])
+    };
 
-    /*
-     *   convert bi_num (BigInt) to hex string then scan it into a new gcry_mpi_t, using gcry_mpi_scan
-     *   copy the new mpi to mpi_ptr
-    */
-    function bigint2mpi(mpi_ptr,bi_num){
-        if(!bigint2mpi.handle) bigint2mpi.handle = _malloc(4);//pointer to *gcry_mpi_t
-        var bi_num_str = BigInt["bigInt2str"](bi_num,16);
-        //gcry_error_t gcry_mpi_scan (gcry_mpi_t *r_mpi, enum gcry_mpi_format format, const unsigned char *buffer, size_t buflen, size_t *nscanned)
-        var err = gcry_.mpi_scan(bigint2mpi.handle,4,bi_num_str,0,0); //4 == HEX Format
-        assert(err===0,"bigint2mpi:"+gcry_.strerror(err));
-    
-        var scanned_mpi_ptr = getValue(bigint2mpi.handle,"i32");
-        if(scanned_mpi_ptr==0){
-            console.log("NULL scanned mpi in bigint2mpi()");
-            process.exit();
-        }
-    
-        var same = gcry_.mpi_set(mpi_ptr,scanned_mpi_ptr);
-        gcry_.mpi_release(scanned_mpi_ptr);
-        if(same && same != mpi_ptr){
-            return same;
-        }
-    }
-
-    gcry_.mpi_new = cwrap('_gcry_mpi_new','number',['number']);
-    gcry_.mpi_set = cwrap('_gcry_mpi_set','number',['number','number']);
-    gcry_.mpi_release = cwrap('_gcry_mpi_release','',['number']);
-    gcry_.mpi_scan = cwrap('_gcry_mpi_scan','number',['number','number','string','number','number']);
-    gcry_.mpi_print = cwrap('_gcry_mpi_print','number',['number','number','number','number','number']);
-    gcry_.strerror = cwrap('_gcry_strerror','string',['number']);
-    
-        __gcry_mpi_mod = globalScope['Module']['__gcry_mpi_mod'] = (function (mpi_r,mpi_x,mpi_n){
-            //r = x mod n
-            var x = mpi2bigint(mpi_x);
-            var n = mpi2bigint(mpi_n);
-            bigint2mpi(mpi_r, BigInt["mod"](x,n));
-        });
-        
-        var original_powm = __gcry_mpi_powm;
-        __gcry_mpi_powm = globalScope['Module']['__gcry_mpi_powm'] = (function (w, b, e, m){
-          var bi_expo = mpi2bigint(e);
-          //elliptic curves exponents 2 or 3, (y^2 = x^3 + ax + b)
-          if(bi_expo[0]==2 || bi_expo[0]==3){
-            original_powm(w,b,e,m); //faster for small exponents.
-            return;
-          }
-          //RSA and DSA use large exponents.. 
-          var bi_base = mpi2bigint(b);
-          var bi_mod  = mpi2bigint(m);
-          var result = BigInt["powMod"](bi_base,bi_expo,bi_mod);
-          bigint2mpi(w,result);
-        });
-
-        //return (x**(-1) mod n) for bigInts x and n.  If no inverse exists, it returns null
-        __gcry_mpi_invm = globalScope['Module']['__gcry_mpi_invm'] = (function (x,a,m){
-            var bi_a = mpi2bigint(a);
-            var bi_m = mpi2bigint(m);
-            var result = BigInt["inverseMod"](bi_a,bi_m);
-            if(result){
-                bigint2mpi(x,result);
-                return 1;
-            }else{
-                return 0;//no inverse mod exists
-            }
-        });
-
-        __gcry_mpi_mulpowm = globalScope['Module']['__gcry_mpi_mulpowm'] = (function (mpi_r,mpi_array_base,mpi_array_exp,mpi_m){
-            var indexer = 1;
-            var mpi1, mpi2, bi_m,bi_result;
-            mpi1 = getValue(mpi_array_base,"i32");
-            mpi2 = getValue(mpi_array_exp,"i32");
-            bi_m = mpi2bigint(mpi_m);
-            var BE = [];
-            var O = [];
-            while(mpi1 && mpi2){
-                BE.push({b:mpi2bigint(mpi1),e:mpi2bigint(mpi2)});
-                mpi1 = getValue(mpi_array_base+(indexer*4),"i32");
-                mpi2 = getValue(mpi_array_exp+ (indexer*4),"i32");
-                indexer++;
-            }
-            if(BE.length){
-                BE.forEach(function(be){
-                    O.push(BigInt["powMod"](be.b,be.e,bi_m));
-                });
-                bi_result = BigInt["str2bigInt"]("1",16);
-                O.forEach(function(k){
-                    bi_result = BigInt["mult"](bi_result,k);
-                });
-            }
-            bi_result = BigInt["mod"](bi_result,bi_m);
-            bigint2mpi(mpi_r,bi_result);
-        });
- 
+   globalScope["__gcry_mpi_powm"] = Module['__gcry_mpi_powm_override'];
+   globalScope["__gcry_mpi_mulpowm"] = Module['__gcry_mpi_mulpowm_override'];
+   globalScope["__gcry_mpi_invm"] = Module['__gcry_mpi_invm_override'];
 });
