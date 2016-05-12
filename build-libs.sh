d0efb13560171a3b4d6bd16d3a9946b997884f8a
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2016-05-12 08:21:14 +0300

    updated to use latest libotr and libgpg-error

--- build-libs.sh
+++ build-libs.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #library versions
-LIBGPG_ERROR_VERSION="1.12"
+LIBGPG_ERROR_VERSION="1.22"
 LIBGCRYPT_VERSION="1.6.2"
-LIBOTR_VERSION="4.1.0"
+LIBOTR_VERSION="4.1.1"
 
 
 #commandline argument
@@ -35,7 +35,7 @@ pushd build
 if [ ! -e "libgpg-error-${LIBGPG_ERROR_VERSION}.tar.bz2" ]
 then
   echo "Downloding libgpg-error-${LIBGPG_ERROR_VERSION}.tar.bz2"
-  curl -O "ftp://ftp.gnupg.org/gcrypt/libgpg-error/libgpg-error-${LIBGPG_ERROR_VERSION}.tar.bz2" 
+  curl -O "ftp://ftp.gnupg.org/gcrypt/libgpg-error/libgpg-error-${LIBGPG_ERROR_VERSION}.tar.bz2"
 fi
 
 # download libgcrypt
@@ -59,10 +59,25 @@ tar xzf "libotr-${LIBOTR_VERSION}.tar.gz"
 #configure and build libgpg-error
 pushd "libgpg-error-${LIBGPG_ERROR_VERSION}"
 BASEDIR=$(dirname $(pwd))
-${EMSCRIPTEN}/emconfigure ./configure --prefix=${BASEDIR} --enable-static --disable-shared --disable-nls --build=x86-unknown-linux --host=x86-unknown-linux --disable-threads --disable-optimization "CFLAGS=-m32"
-mv src/Makefile src/Makefile.original
+${EMSCRIPTEN}/emconfigure ./configure --prefix=${BASEDIR} --enable-static --disable-shared --disable-nls --disable-threads --disable-doc --build=x86-unknown-linux --host=x86-unknown-linux "CFLAGS=-m32"
+cp config.h src/root_config.h
+cp src/Makefile src/Makefile.original
 sed -e 's:\$(CC_FOR_BUILD) -I\. -I\$(srcdir) -o $@:\$(CC_FOR_BUILD) -I. -I\$(srcdir) -o $@.js:' \
-    -e 's:\./mkerrcodes:node ./mkerrcodes.js:' src/Makefile.original > src/Makefile
+    -e 's:\./mkerrcodes:node ./mkerrcodes.js:' \
+    -e 's:gen-posix-lock-obj\$(EXEEXT):gen-posix-lock-obj.js:' \
+    -e 's:Makefile gen-posix-lock-obj posix-lock-obj.h:Makefile gen-posix-lock-obj.js posix-lock-obj.h:' \
+    -e 's:\./gen-posix-lock-obj >\$@:node ./gen-posix-lock-obj.js >\$@:' \
+    -e 's:\$(CC_FOR_BUILD) -g -O0 -I. -I\$(srcdir) -o \$@ \$(srcdir)/mkheader.c:\$(CC_FOR_BUILD) -g -O0 -I. -I\$(srcdir) -o \$@.js \$(srcdir)/mkheader.c --embed-file ./root_config.h --embed-file ./gpg-error.h.in --embed-file ./err-sources.h.in --embed-file ./err-codes.h.in --embed-file ./errnos.in --embed-file ./lock-obj-pub.native.h:' \
+    -e 's:\./mkheader $(host_os) $(host_triplet)  $(srcdir)/gpg-error.h.in:node ./mkheader.js $(host_os) $(host_triplet)  $(srcdir)/gpg-error.h.in:' \
+    -e 's:\.\./config.h \$(PACKAGE_VERSION) \$(VERSION_NUMBER) >\$@:./root_config.h $(PACKAGE_VERSION) $(VERSION_NUMBER) >$@:' \
+    src/Makefile.original > src/Makefile
+
+# Skip tests
+cp Makefile Makefile.original
+sed -e 's:DIST_SUBDIRS = m4 src doc tests po lang:DIST_SUBDIRS = m4 src doc po lang:' \
+    -e 's:SUBDIRS = m4 src \$(doc) tests po \$(lang_subdirs):SUBDIRS = m4 src $(doc) po $(lang_subdirs):' \
+    Makefile.original > Makefile
+
 make clean
 make
 make install
